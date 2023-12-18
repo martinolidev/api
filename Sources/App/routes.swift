6d9d56a81beabba71a9f38a389f2873463096522
Martin Martinez <mtz.martin@proton.me>
2023-12-18 17:34:45 -0600

    Making sure they can't inject other values that may affect security, validating the IP

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,3 +9,23 @@ func routes(_ app: Application) throws {
         
     }
 }
+
+
+func isValidIp(ip: String) -> Bool {
+    //Split the ip into 4 parts to check if the ip is the IPv4 format
+    let parts = ip.split(separator: ".")
+    if parts.count != 4 {
+        return false
+    }
+    //Check if each part is a number valid, IPv4 parts must be between 0 and 255
+    for part in parts {
+        if let number = Int(part) {
+            if number < 0 || number > 255 {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+    return true
+}
